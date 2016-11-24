8c4911ee0a36a77e69024e24a2fac788dd701dd8
saurav-webonise <saurav.senmazumder@webonise.com>
2016-11-24 19:26:29 +0530

    FunctionsAssignment : Changes in names and arrayDetails

---
+++ FunctionsAssignment/arrayContents.php
@@ -0,0 +1,64 @@
+<?php
+
+$organisationDetails = array(
+      10 => array(
+        'name' => 'weboniseLab',
+        'jobRole' => array(
+          '11' => array(
+            'name' => 'developer',
+            'created' => '2016-02-01',
+          ),
+          '12' => array(
+            'name' => 'sr. developer',
+            'created' => '2016-02-10',
+          ),
+        ),
+        'cfa' => array(
+          '11' => array(
+            'name' => 'php',
+            'created' => '2016-03-10',
+          ),
+          '12' => array(
+            'name' => 'ruby',
+            'created' => '2016-04-15',
+          ),
+        )
+      ),
+      11 => array(
+        'name' => 'Hartley Lab',
+        'jobRole' => array(
+          '11' => array(
+            'name' => 'front end',
+            'created' => '2016-03-01',
+          ),
+          '12' => array(
+            'name' => 'design',
+            'created' => '2016-03-10',
+          ),
+        ),
+        'cfa' => array(
+          '11' => array(
+            'name' => 'UI',
+            'created' => '2016-02-01',
+          ),
+          '12' => array(
+            'name' => 'UX',
+            'created' => '2016-01-01',
+          ),
+        )
+      ),
+      15 => array(
+        'name' => 'Hartley Lab',
+        'jobRole' => array(
+          '11' => array(
+            'name' => 'front end',
+            'created' => '2016-03-01',
+          ),
+          '12' => array(
+            'name' => 'design',
+            'created' => '2016-03-10',
+          ),
+        )
+      )
+    );
+?>
--- FunctionsAssignment/arrayDetails.php
+++ FunctionsAssignment/arrayDetails.php
@@ -1,5 +1,6 @@
 <?php          
-  function array_flatten($array) 
+include('arrayContents.php');
+  function arrayFlatten($array) 
   { 
     if (!is_array($array)) 
     { 
@@ -10,7 +11,7 @@
     { 
        if (is_array($value)) 
        { 
-           $arrayList=array_flatten($value);
+           $arrayList=arrayFlatten($value);
            foreach ($arrayList as $listItem) 
            {
                $result[] = $listItem; 
@@ -29,10 +30,10 @@
     header('Content-Type: application/vnd.ms-excel'); 
     header('Content-Disposition: attachment;filename="demo.csv"');
     global $organisationDetails;
-    foreach ($organisationDetails as $file) 
+    foreach ($organisationDetails as $data) 
     {
         $result = [];
-        array_walk_recursive($file, function($item) use (&$result) 
+        array_walk_recursive($data, function($item) use (&$result) 
         {
           if(empty($item))
           {
@@ -49,69 +50,9 @@
     fclose($output);
   } 
 
-  $flattenArray=array_flatten($organisationDetails);
+  
+  $flattenArray=arrayFlatten($organisationDetails);
   arrayToCsv($flattenArray);
 
-  $organisationDetails = array(
-      10 => array(
-        'name' => 'weboniseLab',
-        'jobRole' => array(
-          '11' => array(
-            'name' => 'developer',
-            'created' => '2016-02-01',
-          ),
-          '12' => array(
-            'name' => 'sr. developer',
-            'created' => '2016-02-10',
-          ),
-        ),
-        'cfa' => array(
-          '11' => array(
-            'name' => 'php',
-            'created' => '2016-03-10',
-          ),
-          '12' => array(
-            'name' => 'ruby',
-            'created' => '2016-04-15',
-          ),
-        )
-      ),
-      11 => array(
-        'name' => 'Hartley Lab',
-        'jobRole' => array(
-          '11' => array(
-            'name' => 'front end',
-            'created' => '2016-03-01',
-          ),
-          '12' => array(
-            'name' => 'design',
-            'created' => '2016-03-10',
-          ),
-        ),
-        'cfa' => array(
-          '11' => array(
-            'name' => 'UI',
-            'created' => '2016-02-01',
-          ),
-          '12' => array(
-            'name' => 'UX',
-            'created' => '2016-01-01',
-          ),
-        )
-      ),
-      15 => array(
-        'name' => 'Hartley Lab',
-        'jobRole' => array(
-          '11' => array(
-            'name' => 'front end',
-            'created' => '2016-03-01',
-          ),
-          '12' => array(
-            'name' => 'design',
-            'created' => '2016-03-10',
-          ),
-        )
-      )
-    );
 ?>
 

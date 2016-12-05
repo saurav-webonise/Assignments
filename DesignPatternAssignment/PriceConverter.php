432516f8f3f1f543ad5ad48a529dd9b9c84a17a1
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:26:27 +0530

    proper indentation

--- DesignPatternAssignment/PriceConverter.php
+++ DesignPatternAssignment/PriceConverter.php
@@ -6,16 +6,15 @@
 	class PriceConverter implements Observer{
 		private $currencies;
 		public function __construct() {
-        	$this->currencies = array();
-    	}
-    	public function addCurrency(Currency $currency) {
-        	array_push($this->currencies, $currency);
-    	} 
-    	public function findCurrency() {
-        	foreach ($this->currencies as $currency) {
-            	$currency->findPrice();
-            	
-        	}
-    	}
+        		$this->currencies = array();
+    		}
+    		public function addCurrency(Currency $currency) {
+        		array_push($this->currencies, $currency);
+    		} 
+    		public function findCurrency() {
+        		foreach ($this->currencies as $currency) {
+            			$currency->findPrice();
+        		}
+    		}
 	}
 ?>

52d461355aea23b029d04eb4b92eb0f87c67950b
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:29:44 +0530

    proper indentation and space in string contatenation

--- DesignPatternAssignment/deleteBook.php
+++ DesignPatternAssignment/deleteBook.php
@@ -2,15 +2,15 @@
 	session_start();
 	include 'PostgresConnection.php';  
 	try{
-		$postgresConn=new PostgresConnection;
-		$conn=$postgresConn->connect();$bookName=$_POST['bookName'];
-		$myBook = $bookName.'.txt';
+		$postgresConn = new PostgresConnection;
+		$conn = $postgresConn->connect();$bookName=$_POST['bookName'];
+		$myBook = $bookName . '.txt';
 		unlink($myBook);
-		$sql = "DELETE FROM book_details WHERE book_name = '".$bookName."'";
+		$sql = "DELETE FROM book_details WHERE book_name = '" . $bookName . "'";
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
 		@header("Location:homePage.php");
 	}catch (Exception $e) {
-    	echo 'Caught exception: ',  $e->getMessage(), "\n";
+    		echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
 ?>

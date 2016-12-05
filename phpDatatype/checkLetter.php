57823accacb30e96e60c0d648f78c66bcafa6a70
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 13:27:30 +0530

    variable names in camelCase in checkLetter.php

--- phpDatatype/checkLetter.php
+++ phpDatatype/checkLetter.php
@@ -3,18 +3,14 @@
     $word=$_GET['word'];
     $count=0;
     $pos=null;
-    $letter_array=str_split($word);
-   
-        for($i=0;$i<strlen($word);$i++)
-        {
-            if(strtolower($letter_array[$i])==$input)
-            {
-                    $pos=$i+1;
-                    echo $pos;
-            }   
-        }
-        if($pos==null)
-        {
-                echo "wrong letter";
-        }     
+    $letterArray=str_split($word);
+    for($i=0;$i<strlen($word);$i++){
+        if(strtolower($letterArray[$i])==$input){
+            $pos=$i+1;
+            echo $pos;
+        }   
+    }
+    if($pos==null){
+        echo "wrong letter";
+    }     
 ?>

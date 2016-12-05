8c942c1a44f6834a78e3fb6aa181e4a22c8e1db7
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:33:24 +0530

    spaces in string concatenation

--- DesignPatternAssignment/editBook.php
+++ DesignPatternAssignment/editBook.php
@@ -8,7 +8,7 @@
 	<?php 
 		include 'PostgresConnection.php';  
 		session_start(); 
-		echo "".$_SESSION['userName'];
+		echo "" . $_SESSION['userName'];
 	?></h1>
 	<hr>
 	<form name="editBook" method="POST" action="editBook.php">
@@ -17,20 +17,19 @@
 			@header("Location:homePage.php");
 		}
 		try{
-			$postgresConn=new PostgresConnection;
-			$conn=$postgresConn->connect();
-			$bookName=$_POST['bookName'];
-			$myBook = $bookName.'.txt';
+			$postgresConn = new PostgresConnection;
+			$conn = $postgresConn->connect();
+			$bookName = $_POST['bookName'];
+			$myBook = $bookName . '.txt';
 			$book = fopen($myBook, 'a+');
-			$bookContent=file_get_contents($bookName.'.txt');
-			echo "Book Name : ".$myBook;
-			echo "<br><textarea name='txtAreaBook' id='txtAreaBook' rows='30' cols='100' >".$bookContent."</textarea><br>";
+			$bookContent = file_get_contents($bookName.'.txt');
+			echo "Book Name : " . $myBook;
+			echo "<br><textarea name='txtAreaBook' id='txtAreaBook' rows='30' cols='100' >" . $bookContent . "</textarea><br>";
 			fwrite($book, $bookContent);
 			echo "<input type='submit' name='btnSaveBook' value='Save Book'>";
 		}catch (Exception $e) {
-    		echo 'Caught exception: ',  $e->getMessage(), "\n";
+    			echo 'Caught exception: ',  $e->getMessage(), "\n";
 		}
-		
 	?>
 </form>
 </body>

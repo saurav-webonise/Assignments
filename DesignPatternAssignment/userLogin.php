216f6dcd06655419fb46043b78e365b6ca9f9808
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:39:24 +0530

    proper indentation

--- DesignPatternAssignment/userLogin.php
+++ DesignPatternAssignment/userLogin.php
@@ -1,17 +1,17 @@
 <?php
 	include 'PostgresConnection.php';
 	$flag="false";
-	$userName=$_POST['userName'];
-	$password=$_POST['userPassword'];
+	$userName = $_POST['userName'];
+	$password = $_POST['userPassword'];
 	try{
-		$postgresConn=new PostgresConnection;
-		$conn=$postgresConn->connect();
-		$sqlQuery= "SELECT * FROM user_details WHERE user_name = '".$userName."' and password='".$password."'"; 
+		$postgresConn = new PostgresConnection;
+		$conn = $postgresConn->connect();
+		$sqlQuery = "SELECT * FROM user_details WHERE user_name = '" . $userName . "' and password='" . $password . "'";
 		$statement = $conn->query($sqlQuery);
 		foreach($conn->query($sqlQuery) as $row){
-	    	$flag="true";
+	    	$flag = "true";
 		}
-		if($flag=="true"){
+		if($flag == "true"){
 			session_start();
 			$_SESSION['userName']=$userName;
 			@header("Location:homePage.php");

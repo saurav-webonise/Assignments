b7ddc79bd8470791da0e8656da28caffdc21b8a5
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:28:47 +0530

    spaces during the string contatenation

--- DesignPatternAssignment/createBook.php
+++ DesignPatternAssignment/createBook.php
@@ -2,15 +2,15 @@
 	include 'PostgresConnection.php';
 	session_start();
 	try{
-		$postgresConn=new PostgresConnection;
-		$conn=$postgresConn->connect();
-		$bookName=$_POST['txtBookName'];
-		$bookDescription=$_POST['txtAreaBookDesc'];
+		$postgresConn = new PostgresConnection;
+		$conn = $postgresConn->connect();
+		$bookName = $_POST['txtBookName'];
+		$bookDescription = $_POST['txtAreaBookDesc'];
 		$bookContents=$_POST['txtAreaBookContent'];
-		$bookPrice=$_POST['txtBookPrice'];
+		$bookPrice = $_POST['txtBookPrice'];
 		$myBook = fopen($bookName.".txt", "w");
 		fwrite($myBook, $bookContents);
-		$sql = "INSERT INTO book_details VALUES ('" . $bookName . "', '" . $_SESSION['userName'] . "','". $bookDescription . "','" . $bookPrice . "')";
+		$sql = "INSERT INTO book_details VALUES ('" . $bookName . "', '" . $_SESSION['userName'] . "','" . $bookDescription . "','" . $bookPrice . "')";
     		$conn->exec($sql);
     		@header("Location:homePage.php");
 	}catch (Exception $e) {

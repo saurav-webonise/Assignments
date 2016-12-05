742cf997b49fe3fcdd60c326502204b796d064a1
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:22:35 +0530

    indentation changes and add constant

--- DesignPatternAssignment/Dollar.php
+++ DesignPatternAssignment/Dollar.php
@@ -1,12 +1,13 @@
 <?php
 	require_once 'Currency.php';
 	class Dollar implements Currency{
-		private $price;     
-	    public function __construct($price) {
-	        $this->price = $price;
-	    }     
-	    public function findPrice() {
-	       echo "<br><b>Price In Dollar : ".round($this->price/(68.28),2)." $</b>";
+		private $price;
+		const private dollarPrice=68.32;
+		public function __construct($price){
+	        	$this->price = $price;
+	    	}     
+	    	public function findPrice(){
+	       		echo "<br><b>Price In Dollar : " . round($this->price/(self::dollarPrice),2) . " $</b>";
 		}
 	}
 ?>

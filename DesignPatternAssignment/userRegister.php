08419fcf4b7ce7870ec9aa516caa6caac05fd2db
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:39:57 +0530

    proper indentation

--- DesignPatternAssignment/userRegister.php
+++ DesignPatternAssignment/userRegister.php
@@ -1,17 +1,16 @@
 <?php
 	include 'PostgresConnection.php';
-	$firstName=$_POST['firstName'];
-	$lastName=$_POST['lastName'];
-	$userName=$_POST['userName'];
-	$password=$_POST['userPassword'];
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$userName = $_POST['userName'];
+	$password = $_POST['userPassword'];
 	try{
-		$postgresConn=new PostgresConnection;
-		$conn=$postgresConn->connect();
-		$sql = "INSERT INTO user_details VALUES ('".$firstName."', '".$lastName."', '".$userName."','".$password."')";
+		$postgresConn = new PostgresConnection;
+		$conn = $postgresConn->connect();
+		$sql = "INSERT INTO user_details VALUES ('" . $firstName . "', '" . $lastName . "', '" . $userName . "','" . $password . "')";
 	    $conn->exec($sql);
 	    @header("Location:userLogin.html");	
 	}catch (Exception $e) {
     	echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
-	
 ?>

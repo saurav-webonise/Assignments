523535a4bddb0fb494e1aff2f12c8e392c2608c4
saurav-webonise <saurav.senmazumder@webonise.com>
2016-12-05 14:25:32 +0530

    indentation and add price constant

--- DesignPatternAssignment/Pound.php
+++ DesignPatternAssignment/Pound.php
@@ -1,13 +1,13 @@
 <?php
 	require_once 'Currency.php';
 	class Pound implements Currency{
-		private $price;     
-	    public function __construct($price) {
-	        $this->price = $price;
-	    }     
-	    public function findPrice() {
-	       echo "<br><b>Price In Pound : ".round($this->price/(86.18),2)." &pound;</b>";
-	       
+		private $price;  
+		const private poundPrice=86.18;   
+	    	public function __construct($price){
+	        	$this->price = $price;
+	    	}     
+	    	public function findPrice(){
+	       		echo "<br><b>Price In Pound : " . round($this->price/(self::poundPrice),2) . " &pound;</b>";
 		}
 	}
 ?>
